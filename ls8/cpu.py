4fa8d240f296ec0b7c257861ccceb65b43cca00b
Zac Smith <zrs3141592@gmail.com>
2020-08-07 20:09:35 -0600

    basics

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,22 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.fl = 0
+
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +28,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +57,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -60,6 +69,31 @@ class CPU:
 
         print()
 
+    def get_index(self, binary):
+        binary_str = str(binary)
+        binary_str.replace('0b', '')
+        return int(binary_str, 2)
+
+    def ldi(self, reg_num, value):
+        index = self.get_index(reg_num)
+        self.reg[index] = value
+
+    def prn(self, reg_num):
+        index = self.get_index(reg_num)
+        print(self.reg[index])
+
     def run(self):
         """Run the CPU."""
-        pass
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN = 0b01000111
+
+        while self.ram[self.pc] != HLT:
+            if self.ram[self.pc] == LDI:
+                self.ldi(self.ram[self.pc+1], self.ram[self.pc+2])
+                self.pc += 2
+            elif self.ram[self.pc] == PRN:
+                self.prn(self.ram[self.pc+1])
+                self.pc += 1
+
+            self.pc += 1
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -7,5 +7,7 @@ from cpu import *
 
 cpu = CPU()
 
-cpu.load()
+file = sys.argv[1]
+
+cpu.load(file)
 cpu.run()
