1b960b3393cb0916e52fa5b173c502a412aa2573
Zac Smith <zrs3141592@gmail.com>
2020-08-08 17:51:35 -0600

    set up setPixel

---
+++ chip-8/js/render.js
@@ -0,0 +1,36 @@
+class Render {
+  constructor(scale) {
+    this.cols = 64
+    this.rows = 32
+
+    this.scale = scale
+    this.canvas = document.querySelector('canvas')
+    this.ctx = this.canvas.getContext('2d')
+
+    this.canvas.width = this.cols * this.scale
+    this.canvas.height = this.rows * this.scale
+
+    this.display = new Array(this.cols * this.rows)
+  }
+
+  setPixel(x, y) {
+    if (x > this.cols) {
+      x -= this.cols
+    } else if (x < 0) {
+      x += this.cols
+    }
+
+    if (y > this.rows) {
+      y -= this.rows
+    } else if (y < 0) {
+      y += this.rows
+    }
+
+    let pixelLocation = x + y * this.cols
+    this.display[pixelLocation] ^= 1
+
+    return !this.display[pixelLocation]
+  }
+}
+
+export default Render
