4b817e57fc5468c334195098c1937e1bee60b136
Zac Smith <zrs3141592@gmail.com>
2020-08-12 18:07:41 -0600

    add hex sprites

--- chip-8/js/cpu.js
+++ chip-8/js/cpu.js
@@ -16,6 +16,98 @@ class CPU {
     this.paused = false
     this.speed = 10
   }
+  loadSpritesIntoMemory() {
+    // Array of hex values for each sprite. Each sprite is 5 bytes.
+    // The technical reference provides us with each one of these values.
+
+    const sprites = [
+      0xf0,
+      0x90,
+      0x90,
+      0x90,
+      0xf0, // 0
+      0x20,
+      0x60,
+      0x20,
+      0x20,
+      0x70, // 1
+      0xf0,
+      0x10,
+      0xf0,
+      0x80,
+      0xf0, // 2
+      0xf0,
+      0x10,
+      0xf0,
+      0x10,
+      0xf0, // 3
+      0x90,
+      0x90,
+      0xf0,
+      0x10,
+      0x10, // 4
+      0xf0,
+      0x80,
+      0xf0,
+      0x10,
+      0xf0, // 5
+      0xf0,
+      0x80,
+      0xf0,
+      0x90,
+      0xf0, // 6
+      0xf0,
+      0x10,
+      0x20,
+      0x40,
+      0x40, // 7
+      0xf0,
+      0x90,
+      0xf0,
+      0x90,
+      0xf0, // 8
+      0xf0,
+      0x90,
+      0xf0,
+      0x10,
+      0xf0, // 9
+      0xf0,
+      0x90,
+      0xf0,
+      0x90,
+      0x90, // A
+      0xe0,
+      0x90,
+      0xe0,
+      0x90,
+      0xe0, // B
+      0xf0,
+      0x80,
+      0x80,
+      0x80,
+      0xf0, // C
+      0xe0,
+      0x90,
+      0x90,
+      0x90,
+      0xe0, // D
+      0xf0,
+      0x80,
+      0xf0,
+      0x80,
+      0xf0, // E
+      0xf0,
+      0x80,
+      0xf0,
+      0x80,
+      0x80, // F
+    ]
+
+    // According to the technical reference, sprites are stored in the interpreter section of memory starting at hex 0x000
+    for (let i = 0; i < sprites.length; i++) {
+      this.memory[i] = sprites[i]
+    }
+  }
 }
 
 export default CPU
